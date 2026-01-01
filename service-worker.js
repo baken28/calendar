80d8e55b4e6091b1fd540e448a53a0bd751d07c0
baken28 <81613752+baken28@users.noreply.github.com>
2026-01-01 15:38:40 +0900

    Add via upload
    Add new files

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "habit-calendar-v1";
+const CACHE_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
